2f53598cfb834560f40c8a445e4f011be7c63d35
Sergio Pereira <sergio.pereira@strat.pt>
2012-10-11 15:56:02 +0100

    Fix bug: User data are not accessible on public methods. Fix bug: Event names are not accessible upon first instantiation. Fix bug: No event dispatched upon permissions request. (Login called with permissions scope when user already logged) Add feature: Get permissions.
    	modified:   api/FcbkApi.js

--- api/FcbkApi.js
+++ api/FcbkApi.js
@@ -5,19 +5,16 @@
     		 
     	//singleton
 		var instance;
-     	
-		var constructor; 
-     	
      	     	
-     	constructor = function(appid,modules){
+     	     	
+		var constructor = function(appid,modules){
 				
 			if(instance) return instance;
 			instance  = this;
 			
-			var _self = this;
      		var _modules = modules.split(',');
      		
-     		smp.events.extend(this);
+     		smp.events.extend(instance);
      		
 			(function(d){
 		       var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
@@ -29,39 +26,44 @@
 
 		    // Init the SDK upon load
 		    window.fbAsyncInit = function() {
-		    	
-		      FB.init({
-		        appId      : appid, // App ID
-		        channelUrl : '//'+window.location.hostname+'/channel.txt', // Path to your Channel File
-		        status     : true, // check login status
-		        cookie     : true, // enable cookies to allow the server to access the session
-		        xfbml      : true  // parse XFBML
-		      });
-		      
-		      BuildModules();
-		      
-		      var i,length = _modules.length;
-		      for(i=0; i<length; i++){
-     			FcbkApi.modules[_modules[i]](_self);
-		      }
-		    
-		      
-		      _self.dispatchEvent(FcbkApi.READY);
+
+			    FB.init({
+			        appId      : appid, // App ID
+			        channelUrl : '//'+window.location.hostname+'/channel.txt', // Path to your Channel File
+			        status     : true, // check login status
+			        cookie     : true, // enable cookies to allow the server to access the session
+			        xfbml      : true  // parse XFBML
+			      });
+			      
+			      BuildModules();
+			      
+			      var i,length = _modules.length;
+			      for(i=0; i<length; i++){
+	     			FcbkApi.modules[_modules[i]](instance);
+			      }
+			    
+			      instance.dispatchEvent(FcbkApi.READY);
 		    };
-		    
-     		
-     		
 
      	};
      	
      	
-     	var _user_data   = {id:undefined, name:'', photo:''};
+     	var _user_data;
+     	resetUserData();
+     	
      	function getProfileUrl(id){
      		return "https://graph.facebook.com/"+id+"/picture";
      	}
+     	function resetUserData(){
+     		_user_data = {id:undefined, name:'', photo:'', permissions:''};
+     	}
+     	function userData(){
+     		return _user_data;
+     	}
      	
      	constructor.prototype = {
-     		user_data : _user_data,
+     		userData : userData,
+     		resetUserData:resetUserData,
      		getProfileUrl:getProfileUrl
      	}
      	
@@ -69,62 +71,96 @@
       	
       }()); 
       
+      
+      FcbkApi.READY = 'READY';
+      FcbkApi.LOGIN = 'LOGIN';
+      FcbkApi.LOGOUT = 'LOGOUT';
+      FcbkApi.USER_GET = 'USER_GET';
+      FcbkApi.USERPERMISSIONS_GET = 'USERPERMISSIONS_GET';
+      FcbkApi.POST = 'POST';
+      FcbkApi.FRIENDS_GET = 'FRIENDS_GET';
+      FcbkApi.EVENTS_GET = 'EVENTS_GET';
+      FcbkApi.EVENT_GET = 'EVENT_GET';
+      FcbkApi.EVENT_POST = 'EVENT_POST';
+      FcbkApi.EVENT_DELETE = 'EVENT_DELETE';
+      FcbkApi.FRIENDS_INVITE = 'FRIENDS_INVITE';
+      
       function BuildModules(){
-    	  
-    	  FcbkApi.READY = 'READY';
-	      FcbkApi.LOGIN = 'LOGIN';
-	      FcbkApi.LOGOUT = 'LOGOUT';
-	      FcbkApi.USER_GET = 'USER_GET';
-	      FcbkApi.POST = 'POST';
-	      FcbkApi.FRIENDS_GET = 'FRIENDS_GET';
-	      FcbkApi.EVENTS_GET = 'EVENTS_GET';
-	      FcbkApi.EVENT_GET = 'EVENT_GET';
-	      FcbkApi.EVENT_POST = 'EVENT_POST';
-	      FcbkApi.EVENT_DELETE = 'EVENT_DELETE';
-	      FcbkApi.FRIENDS_INVITE = 'FRIENDS_INVITE';
-	      
-	      
+         
 	      FcbkApi.modules = {};
 	      FcbkApi.modules.auth = function(obj){
+	    	  
+	    	  var _self = obj;
+	    	  
 			  smp.clone({
-	          	login: function(scope){
+	          	login: function(permissions){
 	          		
-	          		FB.login(null, {scope: scope});
+	          		FB.login(onAuthResponse, {scope: permissions});
 	          	},
 	          	logout: function(){FB.logout();},
-	          	getUser:function(){return obj.user_data;}
+	          	getUser:function(){
+	          			if(_self.userData().id == undefined){
+	          				FB.api('/me', onUserReady);
+	          				return null;
+	          			}
+	          			return _self.userData();
+	          	},
+	          	getPermissions:function(updateFromServer){
+	          		if(_self.userData().id != undefined){
+	          			if(_self.userData().permissions == "" || updateFromServer){
+	          				FB.api('/me/permissions', onPermissionsReady);
+	          				return null;
+	          			}else{
+	          				_self.dispatchEvent(FcbkApi.USERPERMISSIONS_GET, _self.userData().permissions);
+	          				return _self.userData().permissions;
+	          				
+	          			}
+          				
+          			}
+          			return null;
+	          	}
 	          }, obj);
 			  
 			  /** listen for and handle auth.statusChange events */
-	          FB.Event.subscribe('auth.statusChange', function(response) {
-	            if (response.authResponse) {
-	              FB.api('/me', onUserReady);
-	              onLogin();
-	            } else {
-	              onLogout();
-	            }
-	          });
+	          FB.Event.subscribe('auth.statusChange', onAuthResponse);
 	          
 			  /** event handlers */
-	          var _self = obj;
+	          	function onAuthResponse(response){
+	          		
+	          		if (response.authResponse) {
+
+	   	              FB.api('/me', onUserReady);
+	   	              onLogin();
+	   	            } else {
+	   	              onLogout();
+	   	            }
+	          	}
 		      	function onLogin(data){
 		      		_self.dispatchEvent(FcbkApi.LOGIN);
 		   		}
-		  		function onUserReady(user){
-		  			_self.user_data.id = user.id;
-		  			_self.user_data.name = user.name;
-		  			_self.user_data.photo = _self.getProfileUrl(user.id);
-		  			_self.dispatchEvent(FcbkApi.USER_GET, user);
+		  		function onUserReady(response){
+
+		  			if(!response.error){
+		  				_self.userData().id = response.id;
+			  			_self.userData().name = response.name;
+			  			_self.userData().photo = _self.getProfileUrl(response.id);
+		  			}
+		  			
+		  			_self.dispatchEvent(FcbkApi.USER_GET, response);
 		       	}
+		  		function onPermissionsReady(response){
+		  			_self.userData().permissions = response.data[0];
+		  			_self.dispatchEvent(FcbkApi.USERPERMISSIONS_GET, response.data[0]);
+		  		}
 		  		function onLogout(){
-		  			_self.user_data = undefined;
+		  			_self.resetUserData();
 		  			_self.dispatchEvent(FcbkApi.LOGOUT);
 		  	    }
 		  }
 	      FcbkApi.modules.friend = function(obj){
 	    	  smp.clone({
 	    		  getFriends:function(){
-	    			  FB.api('https://graph.facebook.com/me/friends', onFriendsReceived);
+	    			  FB.api('/me/friends', onFriendsReceived);
 	    		  }
 	    	  },obj);
 	    	  
@@ -141,10 +177,10 @@
 	    	  //requires scope 'user_events,create_event'
 	    	  smp.clone({
 		    	  getEvents:function(){
-		    		FB.api('https://graph.facebook.com/me/events', onEventsReceived);
+		    		FB.api('/me/events', onEventsReceived);
 		    	  },
 		    	  getEvent:function(id){
-		    		FB.api('https://graph.facebook.com/'+id, onEventReceived);
+		    		FB.api('/'+id, onEventReceived);
 		    	  },
 		    	  postEvent:function(name,description,start,end,location,venue,privacy,picture){
 		    		  	/**
@@ -153,23 +189,31 @@
 						 */
 			    		  
 			    		  var data = {};
+			    		  
 			    		  data.name = name;
 			    		  data.description = description;
 			    		  data.start_time = start;
-			    		  if(end != undefined) data.end_time = end;
+			    		  if(end) data.end_time = end;
 			    		  data.location = location;
-			    		  if(venue != undefined) data.venue = venue;
-			    		  (privacy != undefined) ? data.privacy = privacy : data.privacy = 'SECRET';
-			    		  if(picture != undefined) data.picture = picture;
-			    				 
+			    		  if(venue) data.venue = venue;
+			    		  if(privacy) {
+			    			  data.privacy = privacy;
+			    			  data.privacy_type = privacy;
+			    		  }else{
+			    			 data.privacy = 'SECRET';
+			    			 data.privacy_type = 'SECRET';
+			    		  }
+			    		  if(picture) data.picture = picture;
+			    		  
+
 							
-			    		  FB.api('https://graph.facebook.com/me/events', 'post', data, onEventPost);
+			    		  FB.api('/me/events', 'post', data, onEventPost);
 		    	  },
 		    	  deleteEvent:function(id){
-		    		FB.api('https://graph.facebook.com/'+id, 'delete', onEventDelete);
+		    		FB.api('/'+id, 'delete', onEventDelete);
 		    	  },
 		    	  inviteFriends:function(evtid,friendids){
-		    		  FB.api('https://graph.facebook.com/'+evtid+'/invited?users='+friendids,'post', onFriendsInvited);
+		    		  FB.api('/'+evtid+'/invited?users='+friendids,'post', onFriendsInvited);
 		    		  
 		    	  }
 	    	  }, obj);
@@ -198,13 +242,17 @@
 	      };
 	      
 	      FcbkApi.modules.post = function(obj){
+	    	  
+	    	  var _self = obj;
+	    	  
 	    	  smp.clone({
 	    		  postUI:function(attname, attcaption, attdesc, attlink, attpicture, actionLinks, userPromptMsg, displayType){
 	    			if(displayType == undefined){
 	    					//also 'iframe'
 	    					displayType = "popup";
 	    			}
-	    			if(obj.user_data.id != 'undefined')
+
+	    			if(_self.userData().id != 'undefined')
 	    			{
 	    					//requires scope 'publish_stream'
 	    					FB.ui({
@@ -212,8 +260,8 @@
 	    								method: 'stream.publish',
 	    								attachment: {
 	    									name: attname,
-	    									caption: attcaption,
-	    									description: attdesc,
+	    									caption: attcaption.replace(/<br\s*\/?\>/gi,'<center></center>'),
+	    									description: attdesc.replace(/<br\s*\/?\>/gi,'<center></center>'),
 	    									href: attlink,
 	    									media: [{'type':'image','src':attpicture,'href':attlink}]
 	    								},
@@ -230,7 +278,7 @@
 	    	  },obj);
 	    	  
 	    	  /** event handlers */
-	    	  var _self = obj;
+	    	 
 	    	  function onPostSent(response) {
 					if (!response || response.error || response.post_id == null || response.post_id == "") {
 						_self.dispatchEvent(FcbkApi.POST, {status:-1, response:response});
